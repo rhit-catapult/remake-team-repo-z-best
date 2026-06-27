db74e3bd1ed5fcc2032d441216c90a41c1ac8ae8
rhit-McKinleyFisher <fisher.mckinley@gmail.com>
2026-06-27 10:48:47 -0400

    mkaing the bullet work

--- my_character.py
+++ my_character.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import math
+import peanut_bullet_module
 
 class MainC:
     def __init__(self, screen, x, y, image_filename):
@@ -10,11 +11,12 @@ class MainC:
         self.original_image = pygame.image.load(image_filename).convert_alpha()
         self.image = self.original_image
         self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.bullets = []
+        self.mouse_x, self.mouse_y = pygame.mouse.get_pos()
 
     def update_angle(self):
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        dx = mouse_x - self.x
-        dy = mouse_y - self.y
+        dx = self.mouse_x - self.x
+        dy = self.mouse_y - self.y
         angle = math.degrees(math.atan2(-dy, dx))  # negative dy because pygame y-axis is flipped
 
         # rotate image
@@ -23,9 +25,18 @@ class MainC:
 
     def draw(self):
         self.screen.blit(self.image, self.rect)
-# This function is called when you run this file, and is used to test the Character class individually.
-# When you create more files with different classes, copy the code below, then
-# change it to properly test that class
+        
+    def draw_crosshairs(self):
+        self.mouse_x, self.mouse_y = pygame.mouse.get_pos()
+        crosshair_width = 2
+        crosshair_height = 30
+        pygame.draw.rect(self.screen, pygame.Color("black"), (self.mouse_x - crosshair_width / 2, self.mouse_y - crosshair_height / 2, crosshair_width, crosshair_height))
+        pygame.draw.rect(self.screen, pygame.Color("black"), (self.mouse_x - crosshair_height / 2, self.mouse_y - crosshair_width / 2, crosshair_height, crosshair_width))
+
+    def fire(self):
+        self.bullets.append(peanut_bullet_module.Bullet(self.screen, self.x, self.y))
+
+
 def test_character():
     # TODO: change this function to test your class
     screen = pygame.display.set_mode((640, 480))
