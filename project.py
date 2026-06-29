2bf72ca9d3872b3a1c19c11b4b7d8c145b3201c9
GolloGoneLoco <zacchaeusseals@gmail.com>
2026-06-29 08:22:27 -0700

    mechanics code udated(bullet incorperated)

--- project.py
+++ project.py
@@ -2,27 +2,34 @@ import pygame
 import sys
 import random
 import time
+from peanut_bullet_module import Bullet
 from my_character import MainC
 from zombie_module import Zombie
 
 def main():
-    # turn on pygame
     pygame.init()
     pygame.display.set_caption("peanut apocolypse")
     screen = pygame.display.set_mode((1300, 800))
-    # creates a Character from the my_character.py file
+
     player = MainC(screen, 100, 100, "Character_Placeholder.png")
     zombie = Zombie(screen, 600, 300, "ZombieFIXED.png")
 
-    # let's set the framerate
     clock = pygame.time.Clock()
+
     while True:
-        clock.tick(60)  # this sets the framerate of your game
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                player.fire()
+
         pressed_keys = pygame.key.get_pressed()
-#############################Player Movement###############################
+
+        ############################# Player Movement ###############################
         if pressed_keys[pygame.K_w]:
             player.y -= 5
         if pressed_keys[pygame.K_s]:
@@ -31,24 +38,22 @@ def main():
             player.x -= 5
         if pressed_keys[pygame.K_d]:
             player.x += 5
-        player.update_angle()
-############################################################################
-        zombie.follow_player(player)
+        
+        ############################################################################
         player.mouse_x, player.mouse_y = pygame.mouse.get_pos()
+        player.update_angle()
+        
+        zombie.follow_player(player)
         zombie.update_angle(player)
 
-
-        # TODO: Fill the screen with whatever background color you like!
         screen.fill((255, 255, 255))
+        for bullet in player.bullets:
+            bullet.move()
+            bullet.draw()
 
-        # draws the character every frame
         player.draw()
         zombie.draw()
 
-        # TODO: Add your project code
-
-        # don't forget the update, otherwise nothing will show up!
-        
         pygame.display.update()
 
 main()
